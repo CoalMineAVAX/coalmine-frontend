f495cf184ab4854d4f69cf1a2d400c10a8d5e2e2
momsk <87495588+momsk@users.noreply.github.com>
2021-11-05 20:04:08 -0400

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   chainId: 56,
-  contractAddress: "0x31413D6d45b016bFA03C2e22E3236ca5f69B7463",
+  contractAddress: "0xE2D26507981A4dAaaA8040bae1846C14E0Fb56bF",
   scanLink:
-    "https://www.bscscan.com/address/0x31413D6d45b016bFA03C2e22E3236ca5f69B7463",
+    "https://www.bscscan.com/address/0xE2D26507981A4dAaaA8040bae1846C14E0Fb56bF",
 };
