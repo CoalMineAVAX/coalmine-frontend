442c2d7de52f75639ca32ba8cc582d43df71969f
mrjunying98 <82926228+mrjunying98@users.noreply.github.com>
2022-04-15 00:39:49 +0800

    changes

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
   chainId: 250,
-  contractAddress: "0xE2D26507981A4dAaaA8040bae1846C14E0Fb56bF",
+  contractAddress: "0x7A03eA9b8a876e3D4527675653F9A11D034aebCc",
   scanLink:
-    "https://www.bscscan.com/address/0xE2D26507981A4dAaaA8040bae1846C14E0Fb56bF",
+    "https://ftmscan.com/address/0x7a03ea9b8a876e3d4527675653f9a11d034aebcc#code",
 };
